8dc886dbbd12175141a2d6155e743f669b172f0a
podgortsev <podgortsev@gmail.com>
2025-02-14 22:36:59 -0800

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -93,6 +93,7 @@ def privacypolicy(request):
     response = render(request, 'privacypolicy.html', {})    
     return response
 
+@csrf_exempt
 def agreebtn(request):
     t = term_agree()
     t.user_id = request.COOKIES['user_logged_in']
