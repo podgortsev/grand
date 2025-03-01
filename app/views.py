913075b9cfa94d5481101d7182b898df49b14530
podgortsev <podgortsev@gmail.com>
2025-03-01 12:11:26 -0800

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -191,6 +191,7 @@ def askopenai(msg, user_id, typ):
             except:
                 pass
     new_assistant_name_index = ai_response.find(';')
+    return [new_assistant_name_index,ans_assist]
     new_assistant_name = ai_response[:new_assistant_name_index].strip()
     ans_msg = ai_response[new_assistant_name_index + 1:].strip()
     if new_assistant_name_index!="0":
